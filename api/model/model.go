ed95ed9deb0dae67ef48d05efe5cdd224392772b
Usov A <esivres@gmail.com>
2024-02-19 11:16:36 +0700

    feat: Добавил информацию об ФИО индивидуального предпримимателя https://dadata.ru/api/find-party/

--- api/model/model.go
+++ api/model/model.go
@@ -338,6 +338,7 @@ type (
 		Okogu  string `json:"okogu"`
 		Okfs   string `json:"okfs"`
 		Okved  string `json:"okved"`
+		Fio    FIO    `json:fio`
 		Okveds []*struct {
 			Main bool   `json:"main"`
 			Type string `json:"type"`
